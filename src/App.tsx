2412eb37875aae18b19af9a0687d5fb04ca51c1a
Jean Santos <jeansantos.sw@gmail.com>
2023-01-31 21:53:23 +0000

    format document

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,28 @@ export interface TasksProps {
 }
 
 export function App() {
-  const [tasks, setTasks] = useState<TasksProps[]>([
-  ])
+  const [tasks, setTasks] = useState<TasksProps[]>([])
 
   function addTask(taskName: string) {
     setTasks([...tasks, { nameTask: taskName, isCompleted: false }])
   }
 
   function deleteTask(taskId: string) {
-    const newTasks = tasks.filter(task => { return task.nameTask !== taskId })
+    const newTasks = tasks.filter(task => {
+      return task.nameTask !== taskId
+    })
     setTasks(newTasks)
   }
 
   function toggleTaskCompleted(taskId: string) {
     const newTasks = tasks.map(task => {
-      if(task.nameTask === taskId) {
+      if (task.nameTask === taskId) {
         return {
           ...task,
-          isCompleted: !task.isCompleted
+          isCompleted: !task.isCompleted,
         }
       }
-      return task;
+      return task
     })
     setTasks(newTasks)
   }
@@ -39,7 +40,11 @@ export function App() {
   return (
     <>
       <Header onAddTask={addTask} />
-      <ListTasks tasks={tasks} onDelete={deleteTask} onComplete={toggleTaskCompleted} />
+      <ListTasks
+        tasks={tasks}
+        onDelete={deleteTask}
+        onComplete={toggleTaskCompleted}
+      />
     </>
   )
 }
